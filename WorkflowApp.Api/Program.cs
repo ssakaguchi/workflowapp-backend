4b5151dece7da1a3ee0664fdfcba4d757e87f5f4
Seiya Sakaguchi <s4e2i4ya@gmail.com>
2026-06-05 20:45:02 +0900

    fix: テスト環境では起動時マイグレーションを実行しないようにする

--- WorkflowApp.Api/Program.cs
+++ WorkflowApp.Api/Program.cs
@@ -77,7 +77,11 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    dbContext.Database.Migrate();
+
+    if (dbContext.Database.IsRelational())
+    {
+        dbContext.Database.Migrate();
+    }
 }
 
 if (app.Environment.IsDevelopment())
